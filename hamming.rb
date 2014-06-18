60fd9d257ea797691e4ce5f57c27223c31554912
Layne McNish <lmcnish14@gmail.com>
2014-06-18 14:39:23 -0600

    solo hamming exercise submission

--- hamming.rb
+++ hamming.rb
@@ -2,6 +2,26 @@ require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
 
+strand_comparison = 0
+
+
+  if (strand_1.length != strand_2.length) && (strand_1.length > strand_2.length)
+    array_comparison_size = strand_2.length - 1
+
+  elsif (strand_2.length != strand_1.length ) && (strand_2.length > strand_1.length)
+    array_comparison_size = strand_1.length - 1
+
+  else
+    array_comparison_size = strand_1.length
+  end
+
+(0..array_comparison_size).each do |num|
+  if strand_1[num] != strand_2[num]
+    strand_comparison += 1
+  end
+end
+
+strand_comparison
 end
 
 check("Hamming distance between identical strands",
